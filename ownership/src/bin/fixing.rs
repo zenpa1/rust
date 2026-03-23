04da9cb9a6667e364f5e88834ce651e53e0d1a0f
zenpa1 <zer0zenpa1@gmail.com>
2026-03-23 11:13:46 +0800

    added chapter 4.3 progress

--- ownership/src/bin/fixing.rs
+++ ownership/src/bin/fixing.rs
@@ -12,16 +12,84 @@
 //     full
 // }
 
+// fn main() {
+//     // copy a number out of a vector
+
+//     // initialize v as a vector with i32s
+//     let v: Vec<i32> = vec![0, 1, 2];
+
+//     // initialize n_ref as an i32 reference, pointing to v's first index
+//     let n_ref: &i32 = &v[0];
+
+//     // initialize n with the i32 value found by dereferencing n_ref
+//     // dereferencing just expects the R permission
+//     let n: i32 = *n_ref;
+// }
+
+// fn main() {
+//     // name, name.0, and name.1 get RWO
+//     let mut name: (String, String) = ( // tuple
+//         String::from("Ferris"),
+//         String::from("Rustacean")
+//     );
+
+//      // name and name.0 get R only
+//      // first gets R and O, *first gets R only (borrows name.0)
+//     let first: &String = &name.0;
+
+//     /*
+//     Why is name.1 unaffected? PARTIAL BORROWING
+//     tuple or structs are made of completely separate, distinct physical slots in
+//     memory
+
+//     let first = &name.0 does the ff:
+//     2. name is partially locked (loses O), cannot drop name as it destroys name.0
+//     and it loses W (cannot overwrite whle tuple as it would overwrite name.0)
+//     3. name.1 is completely ignored as it is a disjoint (separate path) from name.0.
+//     Mutating name.1 poses zero threat to memory address that first is pointing to,
+//     thereby keeping its W permission 
+
+//     apartment analogy:
+//     name.0 = bedroom A
+//     name.1 = bedroom B
+
+//     when creating 'first', you are letting a guest stay in bedroom A
+//     - cannot demolish apartment (name loses O)
+//     - cannot remodel bedroom A (name.0 loses W)
+//     - but guest does not care what happens to bedroom B (name.1 keeps W)
+//     */
+
+//     name.1.push_str(", Esq."); // reqs: R and W
+//     println!("{first} {}", name.1);
+// }
+
+// fn main() {
+//     let mut name = (
+//         String::from("Ferris"),
+//         String::from("Rustacean")
+//     );
+
+//     let first = get_first(&name); // removes all indexes W
+//     // even name.0
+
+//     name.1.push_str(", Esq.");
+//     println!("{first} {}", name.1);
+// }
+
+// fn get_first(name: &(String, String)) -> &String {
+//     &name.0
+// }
+
 fn main() {
-    // copy a number out of a vector
+    let mut a = [0, 1, 2, 3];
+    let x = &mut a[1];
+    // unlike tuples, there are no diff places for a[0], a[1], etc.
+    // uses a single place a[_] that represents all indexes of a
 
-    // initialize v as a vector with i32s
-    let v: Vec<i32> = vec![0, 1, 2];
+    *x += 1;
+    // x and *x implicitly drop here
 
-    // initialize n_ref as an i32 reference, pointing to v's first index
-    let n_ref: &i32 = &v[0];
-
-    // initialize n with the i32 value found by dereferencing n_ref
-    // dereferencing just expects the R permission
-    let n: i32 = *n_ref;
+    println!("{a:?}");
+    // :? is a formatting directive that tells Rust to use the
+    // std::fmt::Debug
 }
