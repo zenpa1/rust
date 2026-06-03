5522eb813e0f4c942d2fb82f571edb9eb2ef6bda
zenpa1 <zer0zenpa1@gmail.com>
2026-06-03 10:54:24 +0800

    added hash map samples

---
+++ collections/src/bin/experiment83.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    println!("Blue Score: {}", score);
+}
