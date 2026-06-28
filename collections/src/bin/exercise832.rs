1e49031f106aff36a37d20eb967da548a54438bf
zenpa1 <zer0zenpa1@gmail.com>
2026-06-28 15:06:04 +0800

    added exercise file

---
+++ collections/src/bin/exercise832.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // GOAL: Convert a standard String into Pig Latin
+    /*
+    Rule 1: If a word starts with a consonant, move the first letter to
+            the end and add "ay" (first -> irst-fay)
+
+    Rule 2: If a word starts with a vowel, leave it in place and add "hay"
+            to the end (apple -> apple-hay)
+    */
+
+    let string = String::from("Hello first apple");
+}
+
+fn convert_to_piglatin(original: String) -> String {
+    // Extract the first letter
+    // tip: whitespace separator!!
+
+    // Rule 1 (consonant)
+
+    // Rule 2 (vowel)
+}
