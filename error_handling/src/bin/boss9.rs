0e25779ca1d497d5253647024158b0b85852147a
zenpa1 <zer0zenpa1@gmail.com>
2026-07-23 17:22:34 +0800

    finished chapter 9

--- error_handling/src/bin/boss9.rs
+++ error_handling/src/bin/boss9.rs
@@ -14,13 +14,13 @@ enum LoadingError {
 
 impl From<Error> for LoadingError {
     fn from(err: Error) -> Self {
-        LoadingGame::FileAccessFailed(err)
+        LoadingError::FileAccessFailed(err)
     }
 }
 
 impl From<ParseIntError> for LoadingError {
-    fn from(err: Error) -> Self {
-        LoadingGame::IntegerParseFailed(err)
+    fn from(err: ParseIntError) -> Self {
+        LoadingError::IntegerParseFailed(err)
     }
 }
 
@@ -42,13 +42,21 @@ mod game {
 
             Minion { mana, level }
         }
+
+        pub fn get_mana(&self) -> u32 {
+            self.mana
+        }
+
+        pub fn get_level(&self) -> u32 {
+            self.level
+        }
     }
 }
 
 fn main() {
     match read_minion_data() {
-        Ok(string) => {
-            println!("Success!");
+        Ok(minion) => {
+            println!("Success! The minion has {} mana and is Level {}.", minion.get_mana(), minion.get_level());
         }
         Err(LoadingError::FileAccessFailed(err)) => {
             println!("Failed to access file! {}", err);
@@ -61,11 +69,23 @@ fn main() {
 
 fn read_minion_data() -> Result<Minion, LoadingError> {
     // Read to string to skip byte management
-    fs::read_to_string("minion.txt")
+    let string = fs::read_to_string("minion.txt")?;
     
     // Split by delimiter
-    let parts = string.split(',');
-    let mana: u32 = parts.next().parse::<u32>()?;
-    let level: u32 = parts.next().parse::<u32>()?;
+    let mut parts = string.split(',');
 
+    /*
+    next() -> iterate one element
+    expect() -> unwrap Option, panic! if invalid
+    trim() -> remove invisible whitespace/newline characters
+    parse::<u32>() -> parse to u32 value explicitly
+    ? -> unwrap Result, propagate Err if invalid
+    */
+    let mana: u32 = parts.next().expect("Failed to parse mana.").trim().parse::<u32>()?;
+    let level: u32 = parts.next().expect("Failed to parse level.").trim().parse::<u32>()?;
+
+    // Return new Minion if successfully parsed
+    // Not sure how to explain why it needs Ok(), but I assume it's because
+    // it is part of a Result<T, E>, and Ok() and Err() are their respective wrappers?
+    Ok(Minion::new(mana, level))
 }
