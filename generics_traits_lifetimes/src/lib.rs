ae174148ca8d37defd2cac256c47fae4e5cf0ba9
zenpa1 <zer0zenpa1@gmail.com>
2026-07-27 22:20:25 +0800

    added default impl

--- generics_traits_lifetimes/src/lib.rs
+++ generics_traits_lifetimes/src/lib.rs
@@ -3,8 +3,15 @@
 // }
 
 pub trait Summary {
+    fn summarize_author(&self) -> String;
+
     fn summarize(&self) -> String {
-        String::from("(Read more...)")
+        // Can call other methods in the same trait
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+
+    pub fn notify(item: &impl Summary) {
+        println!("Breaking news! {}", item.summarize());
     }
 }
 
@@ -17,7 +24,16 @@ pub struct NewsArticle {
 
 impl Summary for NewsArticle {
     fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
+        format!(
+            "{}, by {} ({})",
+            self.headline,
+            self.summarize_author(),
+            self.location
+        )
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
     }
 }
 
@@ -35,4 +51,41 @@ pub struct SocialPost {
 // }
 
 // For default implentations, simply have an empty impl block
-impl Summary for SocialPost {}
+impl Summary for SocialPost {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+// fun stuff
+pub trait Character {
+    // required method
+    fn current_hp(&self) -> i32;
+
+    // default method (that applies the required method)
+    fn is_dead(&self) -> bool {
+        self.current_hp() <= 0
+    }
+
+    fn is_bloodied(&self) -> bool {
+        self.current_hp() <= 50
+    }
+
+    fn health_status(&self) -> String {
+        if self.is_dead() {
+            String::from("Deceased")
+        } else {
+            String::from("Alive")
+        }
+    }
+}
+
+pub struct Warrior {
+    pub hp: u32,
+}
+
+impl Character for Warrior {
+    fn current_hp(&self) -> i32 {
+        100
+    }
+}
