f186c38cf6833a171b3c1205a6052ab30e256ea7
zenpa1 <zer0zenpa1@gmail.com>
2026-07-30 17:42:33 +0800

    added returns_summarizable

--- generics_traits_lifetimes/src/lib.rs
+++ generics_traits_lifetimes/src/lib.rs
@@ -107,6 +107,42 @@ pub fn notify(item: &(impl Summary + fmt::Display)) {
     println!("hello world! {item}");
 }
 
+// fn returns_summarizable() -> impl Summary {
+//     // impl Trait in return position
+//     // "return a value of some type that implements said trait"
+//     SocialPost {
+//         username: String::from("horse_ebooks"),
+//         content: String::from("of course, as you probably already know, people",),
+//         reply: false,
+//         repost: false,
+//     }
+// }
+
+fn returns_summarizable(switch: bool) -> impl Summary {
+    if switch {
+        NewsArticle {
+            headline: String::from(
+                "Penguins win the Stanley Cup Championship!",
+            ),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL.",
+            ),
+        }
+    } else {
+        SocialPost {
+            username: String::from("horse_ebooks"),
+            content: String::from(
+                "of course, as you probably already know, people",
+            ),
+            reply: false,
+            repost: false,
+        }
+    }
+}
+
 // fun stuff
 pub trait Character {
     // required method
