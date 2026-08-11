d83fce522c8845c8583b1ec5015721af4750a063
zenpa1 <zer0zenpa1@gmail.com>
2026-08-11 13:53:18 +0800

    added lifetime annotations with scope experiments

--- generics_traits_lifetimes/src/bin/experiment103.rs
+++ generics_traits_lifetimes/src/bin/experiment103.rs
@@ -17,13 +17,44 @@ fn main() {
     let r = &x;
     println!("r: {}", r);
 
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+    // let string1 = String::from("abcd");
+    // let string2 = "xyz";
 
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {result}");
+    // let result = longest(string1.as_str(), string2);
+    // println!("The longest string is {result}");
+
+    // references having different concrete lifetimes
+    let string1 = String::from("long string is long"); // initially 'a
+
+    {
+        let string2 = String::from("xyz"); // the new 'a (smaller)
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {result}"); // valid as it is still within scope
+    }
 }
 
-fn longest(x: &str, y: &str) -> &str {
-    if x.len() > y.len() { x } else { y }
+//     // scope outlive example
+//     let string1 = String::from("long string is long"); // initially 'a
+//     let result;
+
+//     {
+//         let string2 = String::from("xyz"); // the new 'a (smaller)
+//         result = longest(string1.as_str(), string2.as_str());
+//     } // string 2 dropped here while borrowed
+//     println!("the longest string is {result}"); // borrow used here
+//     // but string2 does not live long enough
+// }
+
+// fn longest(x: &str, y: &str) -> &str {
+//     if x.len() > y.len() { x } else { y }
+// }
+
+// // with lifetime annotation
+// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() { x } else { y }
+// }
+
+// what if we just returned x and never y?
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    x
 }
