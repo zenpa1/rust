8d7d4c6fc9dd286d4c5e7e3022327672c343a5c5
zenpa1 <zer0zenpa1@gmail.com>
2026-08-18 20:28:21 +0800

    completed challenge 10.3

--- generics_traits_lifetimes/src/bin/boss103.rs
+++ generics_traits_lifetimes/src/bin/boss103.rs
@@ -1,23 +1,23 @@
 fn main() {
     let transmission = "hello_world";
-    let top_secret_dossier: &str;
+    let top_secret_dossier: Dossier;
 
     {
-        let one_time_pad = "bite of 87";
-        top_secret_dossier = extract(transmission, one_time_pad);
+        let one_time_pad = String::from("bite of 87");
+        top_secret_dossier = extract(transmission, &one_time_pad);
     } // Scope of one_time_pad ends
 
-    println!("{}", top_secret_dossier);
+    println!("{:?}", top_secret_dossier.encrypted_transmission);
 }
 
 struct Dossier<'a> {
     encrypted_transmission: &'a str, // Reference to original document
 }
 
-fn extract<'a, 'b>(encrypted_transmission: &'a str, one_time_pad: &'b str) -> &'a str {
+fn extract<'a, 'b>(encrypted_transmission: &'a str, one_time_pad: &'b str) -> Dossier<'a> { // Dossier<'a>, not &'a Dossier
     // 'a and 'b as it is not related to Dossier
-    // First elision rule infers 'a from &self
-    // Third elision rule infers 'a as output lifetime due to &self
     println!("One-time-pad: {}", one_time_pad);
-    encrypted_transmission
+    Dossier {
+        encrypted_transmission
+    }
 }
