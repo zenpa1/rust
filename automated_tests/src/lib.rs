6ea2e98997a4f267bd1a81da81d240563890f33b
zenpa1 <zer0zenpa1@gmail.com>
2026-09-04 21:20:02 +0800

    added Result testing and clarified expected parameter

--- automated_tests/src/lib.rs
+++ automated_tests/src/lib.rs
@@ -1,24 +1,14 @@
-// pub fn greeting(name: &str) -> String {
-//     format!("Hello {name}!")
-// }
-
-// Bug: The input is not found in the output anymore
 pub fn greeting(name: &str) -> String {
-    format!("Hello!")
+    format!("Hello {name}!")
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// // Bug: The input is not found in the output anymore
+// pub fn greeting(name: &str) -> String {
+//     format!("Hello!")
+// }
 
-    #[test]
-    fn greeting_contains_name() {
-        // The greeting itself may change, but we want to ensure that
-        // it still contains the text of the input parameter
-        let result = greeting("Carol");
-        // Add a second parameter to clarify what the actual output was
-        assert!(result.contains("Carol"), "Greeting did not contain name, value was `{result}`");
-    }
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
 }
 
 pub struct Guess {
@@ -47,17 +37,46 @@ impl Guess {
     pub fn new(value: i32) -> Guess {
         if value < 1 {
             panic!("Guess value must be greater than or equal to 1, got {value}.");
-        } else if value > 100 { // more precise explanation
+        } else if value > 100 {
+            // more precise explanation
             panic!("Guess value must be less than or equal to 100, got {value}.");
         }
 
         Guess { value }
     }
+
+    // // Bug: swap bodies of if statements
+    // pub fn new(value: i32) -> Guess {
+    //     if value < 1 {
+    //         // panic did not contain this expected string
+    //         panic!("Guess value must be less than or equal to 100, got {value}.");
+    //     } else if value > 100 {
+    //         // instead got this, which is not part of the expected parameter
+    //         panic!("Guess value must be greater than or equal to 1, got {value}.");
+
+    //         // it helps us determine if the test suceeded due to the panic! we wanted
+    //         // or if it did another panic! that we are not aware of
+    //     }
+
+    //     Guess { value }
+    // }
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
     use super::*;
+
+    #[test]
+    fn greeting_contains_name() {
+        // The greeting itself may change, but we want to ensure that
+        // it still contains the text of the input parameter
+        let result = greeting("Carol");
+        // Add a second parameter to clarify what the actual output was
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{result}`"
+        );
+    }
 
     // #[test]
     // #[should_panic]
@@ -70,5 +89,17 @@ mod test {
     #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
+    }
+
+    // We can use Result<T, E> to return an Err instead of panicking
+    #[test]
+    fn it_works() -> Result<(), String> {
+        let result = add(2, 2);
+
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
