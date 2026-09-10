13cbc40c4778d8da4bb335e27dc13080bbe94414
zenpa1 <zer0zenpa1@gmail.com>
2026-09-10 12:54:02 +0800

    started Test B

--- combat_engine/src/lib.rs
+++ combat_engine/src/lib.rs
@@ -1,3 +1,4 @@
+// Test A Materials
 pub fn heal(current_hp: u32, max_hp: u32, healing: u32) -> u32 {
     // Simple version of saturated addition
     if current_hp + healing > max_hp {
@@ -5,6 +6,22 @@ pub fn heal(current_hp: u32, max_hp: u32, healing: u32) -> u32 {
     } else {
         current_hp + healing
     }
+}
+
+// Test B Materials
+enum Status {
+    Burn(u32),
+    Poison(u32),
+}
+
+struct Undead {
+    hp: u32,
+    resistance: Status,
+    weakness: Status,
+}
+
+pub fn dmg(hp: u32, dmg: u32) -> u32 {
+    hp - dmg // Unsigned integers can never be less than 0
 }
 
 #[cfg(test)]
@@ -21,5 +38,17 @@ mod tests {
         // Heal test
         let new_health = heal(hp, max_hp, health_potion);
         assert_eq!(new_health, 100); // Assert that it is the max value
+    }
+
+    // Test B: Undead is immune to poison
+    #[test]
+    fn poison_immunity() {
+        let undead: Undead = Undead {
+            hp: 50,
+            resistance: Status::Poison(0), // Enums designed to hold a value (u32)
+            weakness: Status::Burn(15),
+        };
+
+        match undead.resistance {}
     }
 }
